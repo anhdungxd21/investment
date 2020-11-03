7e3a121875cfa4672eaffa7b8407136566c90091
= <=>
2020-11-03 17:40:04 +0700

    done

---
+++ src/Investment.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Investment {
+    public static void main(String[] args) {
+        double money = 1.0;
+        int month = 1;
+        double intersetRate = 1.0;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter investment amount: ");
+        money = sc.nextDouble();
+        System.out.print("Enter number of month: ");
+        month = sc.nextInt();
+        System.out.print("Enter annual interest rate in percentage: ");
+        intersetRate = sc.nextDouble();
+        double totalInterset = 0.0;
+        for (int i = 0; i < month; i++) {
+            totalInterset += money *(intersetRate/100)/12*month;
+        }
+        System.out.println("Total of interset: ");
+    }
+}
